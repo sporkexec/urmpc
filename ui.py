9037f7bb52a2608152d185abc3d09bc27c7f9988
Jacob Courtneay <jacob@sporkexec.com>
2011-08-17 23:56:38 -0700

    Adding tracks column, adding many hotkeys.
    As far as basic use goes, we can do pretty much all the major
    operations. We can't edit or view the playlist, but most flags and
    prev/next/pause/etc operations work perfectly.
    
    So far I'm pretty much cloning ncmpcpp, since it gives me a good
    concrete direction to work towards. Once the code settles and the
    FIXMEs and TODOs start fading away, I can start with the big ideas.
    
    Examples of big ideas:
     - Better lyrics viewer, also with mass downloading
     - Customizable everything, although ncmpcpp will have me beat by a
       long shot for a very long time.
     - Good documentation. This necessarily comes after the code has
       settled. One of my big problems with ncmpcpp is that it's filled to
       the brim with features, but much of it is poorly documented. The
       status flags, for example, are a great idea but there's nowhere that
       tells you what each of them are. Also, I really like to write long
       blocks of "condescendingly terse"
       (http://teddziuba.com/2010/08/too-smart-for-git.html) text and this
       sounds like a great opportunity to practice.

--- ui.py
+++ ui.py
@@ -1,10 +1,13 @@
 import urwid
 
+def sends_signal(*signals):
+	def classmaker(cls):
+		urwid.register_signal(cls, signals)
+		return cls
+	return classmaker
+
+
 class IOWalker(urwid.ListWalker):
-	emptytext = '[None]'
-	def __new__(cls, *args, **kwargs):
-		urwid.register_signal(cls, ['change'])
-		return super(IOWalker, cls).__new__(cls, *args, **kwargs)
 	def __init__(self):
 		self.focus = 0
 		self.items = []
@@ -14,7 +17,6 @@ class IOWalker(urwid.ListWalker):
 	def set_focus(self, focus):
 		self.focus = focus
 		self._modified()
-		urwid.emit_signal(self, 'change', self.items[focus])
 	def get_next(self, pos):
 		return self._get_at_pos(pos + 1)
 	def get_prev(self, pos):
@@ -23,15 +25,10 @@ class IOWalker(urwid.ListWalker):
 		if pos < 0:
 			return None, None
 		try:
-			text = self.items[pos]
+			ret = self.items[pos]
 		except IndexError as e:
 			return None, None
-		if text == '':
-			text = self.emptytext
-		item = urwid.Text(text)
-		item.set_wrap_mode('clip')
-		item = self._attrmap(item)
-		return item, pos
+		return ret, pos
 	def _reload(self):
 		"""Grab items from datastore and apply any changes, attempting to
 		preserve focus on current item if possible."""
@@ -48,20 +45,37 @@ class IOWalker(urwid.ListWalker):
 	def _attrmap(self, w):
 		return w
 
+@sends_signal('change')
 class ArtistWalker(IOWalker):
 	def __init__(self, io):
 		self.io = io
 		super(ArtistWalker, self).__init__()
+	def set_focus(self, focus):
+		super(ArtistWalker, self).set_focus(focus)
+		urwid.emit_signal(self, 'change', self.items[focus])
 	def _get_items(self):
 		return sorted(self.io.list('artist'))
 	def _attrmap(self, w):
 		return urwid.AttrMap(w, 'ArtistWalker_main', 'ArtistWalker_focus')
+	def _get_at_pos(self, pos):
+		item, pos = super(ArtistWalker, self)._get_at_pos(pos)
+		if item is None or pos is None:
+			return item, pos
+		if item == '':
+			item = '[None]'
+		item = urwid.Text(item)
+		item.set_wrap_mode('clip')
+		return self._attrmap(item), pos
 
+@sends_signal('change')
 class AlbumWalker(IOWalker):
 	def __init__(self, io, artist):
 		self.io = io
 		self.artist = artist
 		super(AlbumWalker, self).__init__()
+	def set_focus(self, focus):
+		super(AlbumWalker, self).set_focus(focus)
+		urwid.emit_signal(self, 'change', (self.artist, self.items[focus]))
 	def _get_items(self):
 		return sorted(self.io.list('album', 'artist', self.artist))
 	def _attrmap(self, w):
@@ -69,6 +83,42 @@ class AlbumWalker(IOWalker):
 	def change_artist(self, value):
 		self.artist = value
 		self._reload()
+	def _get_at_pos(self, pos):
+		item, pos = super(AlbumWalker, self)._get_at_pos(pos)
+		if item is None or pos is None:
+			return item, pos
+		if item == '':
+			item = '[None]'
+		item = urwid.Text(item)
+		item.set_wrap_mode('clip')
+		return self._attrmap(item), pos
+
+class TrackWalker(IOWalker):
+	def __init__(self, io, artist, album):
+		self.io = io
+		self.artist = artist
+		self.album = album
+		super(TrackWalker, self).__init__()
+	def _get_items(self):
+		#TODO: Make sure this sorts like it should.
+		return self.io.find('artist', self.artist, 'album', self.album)
+	def _attrmap(self, w):
+		return urwid.AttrMap(w, 'TrackWalker_main', 'TrackWalker_focus')
+	def change_album(self, artist_album):
+		self.artist, self.album = artist_album
+		self._reload()
+
+	def _get_at_pos(self, pos):
+		item, pos = super(TrackWalker, self)._get_at_pos(pos)
+		if item is None or pos is None:
+			return item, pos
+		text = item['title']
+		if text == '':
+			text = '[None]'
+		text = urwid.Text(text)
+		text.set_wrap_mode('clip')
+		return self._attrmap(text), pos
+
 
 class TreeList(urwid.ListBox):
 	keymap = {
--- urmpc.py
+++ urmpc.py
@@ -5,12 +5,15 @@ import urwid
 
 import iompd
 import ui
+import globalkeys
 
 palette = [
 	('ArtistWalker_main', 'white', 'black'),
 	('ArtistWalker_focus', 'light cyan', 'black'),
 	('AlbumWalker_main', 'white', 'black'),
 	('AlbumWalker_focus', 'light cyan', 'black'),
+	('TrackWalker_main', 'white', 'black'),
+	('TrackWalker_focus', 'light cyan', 'black'),
 ]
 
 io = iompd.MPDClient()
@@ -22,10 +25,17 @@ artists = ui.TreeList(artist_walker)
 album_walker = ui.AlbumWalker(io, None)
 albums = ui.TreeList(album_walker)
 
+track_walker = ui.TrackWalker(io, None, None)
+tracks = ui.TreeList(track_walker)
+
 urwid.connect_signal(artist_walker, 'change', album_walker.change_artist)
+urwid.connect_signal(album_walker, 'change', track_walker.change_album)
 artist_walker.set_focus(artist_walker.focus) # Force a change event
 
-main = urwid.Columns((artists, albums))
-loop = urwid.MainLoop(main, palette)
+#FIXME: Put everything in a frame or something, avoid the global keybindings.
+keyhandler = globalkeys.GlobalKeys(io)
+
+main = urwid.Columns((artists, albums, tracks))
+loop = urwid.MainLoop(main, palette, unhandled_input=keyhandler.keypress)
 loop.run()
 
