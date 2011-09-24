b73b7d653a1bba1e7cb12693f91fe397c2d0631a
Jacob Courtneay <jacob@sporkexec.com>
2011-09-23 20:14:11 -0700

    Adds Python 2.6 compatibility.
    This is as old as I plan on supporting. Simply put, anybody running
    Debian Lenny or Red Hat probably isn't interested in this anyways.

---
+++ lib/urmpclib/__init__.py
@@ -0,0 +1,36 @@
+if __name__ == '__main__':
+	import os.path, sys
+	sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+	import urwid
+	import urmpd
+	import signals
+	from ui_main import MainFrame
+	import configuration
+	from configuration import config
+
+	config.read('urmpclib/urmpc.conf.example')
+	palette = configuration.extract_palette(config, 'palette')
+	mpc = urmpd.MPDClient()
+	mpc.connect(config.mpd.host, int(config.mpd.port))
+	event_loop = urwid.SelectEventLoop()
+
+	# Get urwid set up
+	#FIXME: Passing None in here is ugly and will eventually break if urwid decides
+	#       to be more strict about it.
+	loop = urwid.MainLoop(None, palette, event_loop=event_loop)
+	signals._mainloop = loop
+
+	# Main widget uses mpd
+	frame = MainFrame(mpc)
+	loop.widget = frame
+
+	# Idler runs cloned mpc connection, uses MainLoop to force redraw on MPD events
+	idler = urmpd.Idler(mpc, loop)
+	event_loop.watch_file(idler, idler)
+
+	try:
+		loop.run()
+	except KeyboardInterrupt as e:
+		pass
+
